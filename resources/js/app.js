672402ff9ab0eb98f635d53a9b57e15dfc5b764a
Vendoo <sh.mestiashvili@vendoo.ge>
2022-04-06 10:22:05 +0400

    i18n

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,13 @@ import {createApp} from "vue";
 import store  from './store/store';
 import router from "./router";
 import App from'./components/App.vue';
+import i18n from './i18n';
+
 
 const app = createApp({});
 app.use(store)
 app.use(router)
-
+app.use(i18n)
 
 
 app.component('app', App);
